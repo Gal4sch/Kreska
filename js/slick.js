4a75009974920b9054d2f5896574f16579e92af6
Gal <marcin.g366@gmail.com>
2024-06-27 22:40:04 +0200

    Added loading animation

--- js/slick.js
+++ js/slick.js
@@ -1,11 +1,10 @@
 $('.hero-carousel').slick({
 	fade: true,
 	infinite: true,
-	speed: 500,
 	fade: true,
 	cssEase: 'linear',
 	autoplay: true,
-	autoplaySpeed: 1000,
+	autoplaySpeed: 2000,
 	speed: 2000,
 	arrows: false,
 	mobileFirst: true,
